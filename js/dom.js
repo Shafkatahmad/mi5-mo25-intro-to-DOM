16665b5c9c98130e302d3a7303ebcee58a9abc46
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-23 22:38:36 +0600

    DOM --> html acts as a document object in js file

---
+++ js/dom.js
@@ -0,0 +1,10 @@
+console.log('Hello from js');
+
+/**
+ * document acts as a type of object in this js file.
+ * In this file, the whole html file is an document object. 
+ * inside the html document object, the "body" tag is a property of the html document object.
+ * and all the h1, p, ul>li, div are the value of the "body property" of the "html document".
+ */
+
+console.log(document.body);

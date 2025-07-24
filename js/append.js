ee41d716bb051b7f6dfa2312e4f6f19b3ea7dbac
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-24 12:36:34 +0600

    creat html element in js using appendChild

---
+++ js/append.js
@@ -0,0 +1,47 @@
+// 1. where to add
+const placesList = document.getElementById('places-list');
+// 2. what to be added
+const li = document.createElement('li');
+li.innerText = 'pahartoli bon';
+// 3. add the child
+placesList.appendChild(li);
+
+// 1. where to add 
+const mainContainer = document.getElementById('main-container');
+
+// 2. what to be added
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'My food list';
+section.appendChild(h1);
+
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.innerText = 'biriani';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li');
+li2.innerText = 'borhani';
+ul.appendChild(li2);
+
+const li3 = document.createElement('li');
+li3.innerText = 'salad';
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);
+
+
+// set innerHTML directly
+const sectionDress = document.createElement('section');
+sectionDress.innerHTML = `
+<h1>My Dress Section</h1>
+<ul>
+    <li>T-shirt</li>
+    <li>Pant</li>
+    <li>sneakers</li>
+</ul>
+`
+
+mainContainer.appendChild(sectionDress);
